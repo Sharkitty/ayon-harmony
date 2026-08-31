d088af6d0ef8ed65090e8015117aa0fe1b6ba8cf
sharkitty <hayleylalicorne@gmail.com>
2026-08-31 14:37:39 +0200

    Bugfix: Add review in supported product types

--- client/ayon_harmony/plugins/load/load_bitmap_drawing.py
+++ client/ayon_harmony/plugins/load/load_bitmap_drawing.py
@@ -135,7 +135,7 @@ class ImportImageLoader(load.LoaderPlugin):
     """Import a single image as a Toon Boom bitmap drawing."""
 
     label = "Import bitmap drawing"
-    product_base_types = {"image", "render", "plate"}
+    product_base_types = {"image", "render", "review", "plate"}
     product_types = product_base_types
     representations = {"*"}
     extensions = {"png", "jpg", "jpeg", "tga", "sgi", "psd"}
